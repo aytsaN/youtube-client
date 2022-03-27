9e01119c27c6c0dd0cf281fc25d2b6f432466052
Anastasiya Shamal <anastasiya_shamal@epam.com>
2022-03-28 01:18:09 +0200

    init: create ISearchItem and ISearchResponse interfaces

---
+++ youtube-client/src/app/search/models/search-item.model.ts
@@ -0,0 +1,44 @@
+interface ISearchItemThumbnail {
+  url: string,
+  width: number,
+  height: number,
+}
+
+interface ISearchItemStatistics {
+  viewCount: string,
+  likeCount: string,
+  dislikeCount: string,
+  favoriteCount: string,
+  commentCount: string,
+}
+
+interface ISearchItemSnippet {
+  publishedAt: string,
+  channelId: string,
+  title: string,
+  description: string,
+  thumbnails: {
+    default: ISearchItemThumbnail,
+    medium: ISearchItemThumbnail,
+    high: ISearchItemThumbnail,
+    standard: ISearchItemThumbnail,
+    maxres: ISearchItemThumbnail,
+  }
+  channelTitle: string,
+  tags: string[],
+  categoryId: string,
+  liveBroadcastContent: string,
+  localized: {
+    title: string,
+    description: string,
+  },
+  defaultAudioLanguage: string,
+  statistics: ISearchItemStatistics,
+}
+
+export interface ISearchItem {
+  kind: string,
+  etag: string,
+  id: string,
+  snippet: ISearchItemSnippet,
+}
